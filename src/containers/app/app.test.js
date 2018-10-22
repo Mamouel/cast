f1232db01f9e740b9424d9cb6eb96e3750889cb8
mamouel <e-dodier@hotmail.fr>
2018-10-22 11:08:42 -0400

    tests initialized

--- src/containers/app/app.test.js
+++ src/containers/app/app.test.js
@@ -1,9 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./app";
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import App from './App';
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('renders without crashing', () => {
+  shallow(<App />);
 });
+
+describe('App component', () => {
+  test('should shallow correctly', () => {
+      expect(shallow(
+        <App />
+      )).toMatchSnapshot() 
+  })
+  test('should mount correctly', () => {
+      expect(mount(
+        <Router>
+          <App />
+        </Router>
+      )).toMatchSnapshot() 
+  })
+  test('should render correctly', () => {
+      expect(render(
+        <Router>
+          <App />
+        </Router>
+      )).toMatchSnapshot() 
+  })
+})
